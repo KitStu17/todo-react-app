965ae9a6e129cfe864da0e88286047cac93feff9
jonghyeon <kiri6077@naver.com>
2023-09-26 10:52:58 +0900

    프론트엔드 개발-8 ~ 11 React로 목록 CRUD 실습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo";
+import React from "react";
+import { Paper, List, Container } from "@material-ui/core";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: "todo0", title: "Todo 1", done: true },
+        { id: "todo1", title: "Todo 2", done: false },
+      ],
+    };
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    // console.log("items: ", this.state.items);
+  };
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      // console.log("Update Items : ", this.state.items);
+    });
+  };
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+        </Container>
+        <div className="TodoList">{todoItems}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
